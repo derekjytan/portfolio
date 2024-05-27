ef62285653193f670d70a8a061daef0c8ddb3e9d
derektan12 <derektan101@gmail.com>
2024-05-27 15:09:34 -0400

    updating build

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  output: 'export',
   plugins: [react()],
   assetsInclude: ['**/*.glb']
 })
