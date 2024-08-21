629432da120a17eec4e6645e8d18cd216b1a1c07
derektan12 <derektan101@gmail.com>
2024-08-21 17:38:33 -0400

    removing some old stuff

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,9 +1,3 @@
-import hero from './hero.jpg'
-import meta from './meta.png'
-import shopify from './shopify.png'
-import starbucks from './starbucks.png'
-import tesla from './tesla.png'
-import logo from './logo.svg'
 import grandtouring from './grandtouring.png'
 import robarts from './robarts.png'
 import predictify from './predictify.png'
@@ -14,12 +8,6 @@ import mely from './mely.jpg'
 import flipthatsheet from './flipthatsheet.png'
 
 export {
-    hero,
-    meta,
-    shopify,
-    starbucks,
-    tesla,
-    logo,
     grandtouring,
     robarts,
     predictify,
