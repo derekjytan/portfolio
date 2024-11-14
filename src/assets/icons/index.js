e4be601d226d045a6e50311881ec24fde17a9859
derektan12 <derektan101@gmail.com>
2024-11-13 22:34:36 -0500

    adding new experience

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,105 +1,107 @@
-import css from './css.svg'
-import express from './express.svg'
-import git from './git.svg'
-import github from './github.svg'
-import html from './html.svg'
-import javascript from './javascript.svg'
-import mongodb from './mongodb.svg'
-import motion from './motion.svg'
-import mui from './mui.svg'
-import nextjs from './nextjs.svg'
-import nodejs from './nodejs.svg'
-import react from './react.svg'
-import redux from './redux.svg'
-import sass from './sass.svg'
-import tailwindcss from './tailwindcss.svg'
-import typescript from './typescript.svg'
-import linkedin from './linkedin.svg'
-import twitter from './twitter.svg'
-import car from './car.svg'
-import estate from './estate.svg'
-import pricewise from './pricewise.svg'
-import snapgram from './snapgram.svg'
-import summiz from './summiz.svg'
-import threads from './threads.svg'
-import arrow from './arrow.svg'
-import contact from './contact.svg'
-import soundon from './soundon.png'
-import soundoff from './soundoff.png'
-import python from './python.svg'
-import java from './java.svg'
-import kotlin from './kotlin.svg'
-import figma from './figma.svg'
-import matlab from './matlab.svg'
-import csharp from './csharp.svg'
-import cplus from './cplus.svg'
-import unity from './unity.svg'
-import blender from './blender.svg'
-import pytorch from './pytorch.svg'
-import pandas from './pandas.svg'
-import earth from './earth-icon.png'
-import aws from './aws.svg'
-import flask from './flask.svg'
-import firebase from './firebase.svg'
-import tensorflow from './tensorflow.svg'
-import keras from './keras.svg'
-import opencv from './opencv.svg'
-import numpy from './numpy.svg'
-import scikitlearn from './scikitlearn.svg'
-import jupyter from './jupyter.svg'
-import mediapipe from './mediapipe.svg'
-import redis from './redis.svg'
+import css from "./css.svg";
+import express from "./express.svg";
+import git from "./git.svg";
+import github from "./github.svg";
+import html from "./html.svg";
+import javascript from "./javascript.svg";
+import mongodb from "./mongodb.svg";
+import motion from "./motion.svg";
+import mui from "./mui.svg";
+import nextjs from "./nextjs.svg";
+import nodejs from "./nodejs.svg";
+import react from "./react.svg";
+import redux from "./redux.svg";
+import sass from "./sass.svg";
+import tailwindcss from "./tailwindcss.svg";
+import typescript from "./typescript.svg";
+import linkedin from "./linkedin.svg";
+import twitter from "./twitter.svg";
+import car from "./car.svg";
+import estate from "./estate.svg";
+import pricewise from "./pricewise.svg";
+import snapgram from "./snapgram.svg";
+import summiz from "./summiz.svg";
+import threads from "./threads.svg";
+import arrow from "./arrow.svg";
+import contact from "./contact.svg";
+import soundon from "./soundon.png";
+import soundoff from "./soundoff.png";
+import python from "./python.svg";
+import java from "./java.svg";
+import kotlin from "./kotlin.svg";
+import figma from "./figma.svg";
+import matlab from "./matlab.svg";
+import csharp from "./csharp.svg";
+import cplus from "./cplus.svg";
+import unity from "./unity.svg";
+import blender from "./blender.svg";
+import pytorch from "./pytorch.svg";
+import pandas from "./pandas.svg";
+import earth from "./earth-icon.png";
+import aws from "./aws.svg";
+import flask from "./flask.svg";
+import firebase from "./firebase.svg";
+import tensorflow from "./tensorflow.svg";
+import keras from "./keras.svg";
+import opencv from "./opencv.svg";
+import numpy from "./numpy.svg";
+import scikitlearn from "./scikitlearn.svg";
+import jupyter from "./jupyter.svg";
+import mediapipe from "./mediapipe.svg";
+import redis from "./redis.svg";
+import meta from "./meta.svg";
 
 export {
-    css,
-    express,
-    git,
-    github,
-    html,
-    javascript,
-    mongodb,
-    motion,
-    mui,
-    nextjs,
-    nodejs,
-    react,
-    redux,
-    sass,
-    tailwindcss,
-    typescript,
-    linkedin,
-    twitter,
-    car,
-    estate,
-    pricewise,
-    snapgram,
-    summiz,
-    threads,
-    arrow,
-    contact,
-    soundon,
-    soundoff,
-    python,
-    java,
-    kotlin,
-    figma,
-    matlab,
-    csharp,
-    cplus,
-    unity,
-    blender,
-    pytorch,
-    pandas,
-    earth,
-    aws,
-    flask,
-    firebase,
-    tensorflow,
-    keras,
-    opencv,
-    numpy,
-    scikitlearn,
-    jupyter,
-    mediapipe,
-    redis
-}
+  css,
+  express,
+  git,
+  github,
+  html,
+  javascript,
+  mongodb,
+  motion,
+  mui,
+  nextjs,
+  nodejs,
+  react,
+  redux,
+  sass,
+  tailwindcss,
+  typescript,
+  linkedin,
+  twitter,
+  car,
+  estate,
+  pricewise,
+  snapgram,
+  summiz,
+  threads,
+  arrow,
+  contact,
+  soundon,
+  soundoff,
+  python,
+  java,
+  kotlin,
+  figma,
+  matlab,
+  csharp,
+  cplus,
+  unity,
+  blender,
+  pytorch,
+  pandas,
+  earth,
+  aws,
+  flask,
+  firebase,
+  tensorflow,
+  keras,
+  opencv,
+  numpy,
+  scikitlearn,
+  jupyter,
+  mediapipe,
+  redis,
+  meta,
+};
