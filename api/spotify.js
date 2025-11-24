42c9a6f5e4d0d97cb668b3a8b3a0bff9b09193d7
derektan12 <derektan101@gmail.com>
2025-11-24 00:18:30 -0500

    fixing top tracks for spotify

--- api/spotify.js
+++ api/spotify.js
@@ -56,6 +56,27 @@ export default async function handler(req, res) {
         success: true,
         items: topArtistsData.items,
       });
+    } else if (req.query.type === "top-tracks") {
+      const topTracksResponse = await fetch(
+        "https://api.spotify.com/v1/me/top/tracks?limit=5&time_range=short_term",
+        {
+          headers: {
+            Authorization: `Bearer ${userAccessToken}`,
+          },
+        }
+      );
+
+      if (!topTracksResponse.ok) {
+        throw new Error(
+          `Failed to fetch top tracks: ${topTracksResponse.status}`
+        );
+      }
+
+      const topTracksData = await topTracksResponse.json();
+      return res.status(200).json({
+        success: true,
+        items: topTracksData.items,
+      });
     }
 
     // Now use this token to get currently playing track
